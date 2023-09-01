4f71898b65310dda3d5d83e05b27512c589043e4
misya11p <miya33ps@gmail.com>
2023-09-01 16:02:56 +0900

    update

--- src/js/options.js
+++ src/js/options.js
@@ -7,6 +7,25 @@ function saveOptions() {
   });
 }
 
+async function setGithubUserId() {
+  chrome.storage.sync.get({
+    githubUserId: ""
+  }, function(items) {
+    document.querySelector("#github-user-id").value = items.githubUserId;
+  });
+}
+
+async function setColor() {
+  chrome.storage.sync.get({
+    skinColor: "yellow"
+  }, function(items) {
+    document.querySelector("#skin-color").value = items.skinColor;
+  });
+}
+
 document.addEventListener('DOMContentLoaded', function() {
   document.getElementById('save').addEventListener('click', saveOptions);
 });
+
+setGithubUserId();
+setColor();
